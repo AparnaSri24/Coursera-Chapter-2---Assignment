265760efd7832e3a09d603c1130a2c6cfdb81515
Aparna Srivastava <163415621+AparnaSri24@users.noreply.github.com>
2024-03-19 10:24:14 +0100

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#   # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+# 5. Scala
+# 6. Java
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. NumPy
+# 2. Pandas
+# 3. Matplotlib
+# 4. Seaborn
+
+# |Data Science Tools|
+# ------------------|
+# |Jupyter Notebooks|
+# |RStudio|
+# |Spyder|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[5]:
+
+
+(3*4)+5
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+# In[13]:
+
+
+minutes = 200
+Hours = minutes / 60
+print(f"{minutes} minutes is equal to {Hours} hours")
+
+
+# _This will convert 200 minutes to hours by diving by 60_
+# 
+
+# Some of the key takeaways from the course
+# 
+# **Objectives:**
+
+# List popular languages for Data Science
+# 
+# * Python
+# * R
+# * SQL
+# * Julia
+# * Scala
+# * Java
+
+# ## Author
+# Aparna Srivastava
+
+# In[ ]:
+
+
+
+
